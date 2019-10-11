5bd373dda6142791ae7e1563d7a1d2858c11ec5e
David Arinder <david.arinder@gmail.com>
2019-10-11 14:20:36 -0700

    app.js experimenting

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,56 @@
 'use strict';
 
 console.log('App.js is running!');
+//to run during dev
+//babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
+//live-server public
 
 // JSX - JavaScript XML
-var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
+var template = // wrapping parantheses only for readability, to remove an open-ended '='
+React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Indecision App'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is some cool info'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
+    )
 );
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'David Arinder'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: Seattle'
+    )
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot); // render this element (template) into this part of the browser (appRoot)
+ReactDOM.render(templateTwo, appRoot); // render this element (template) into this part of the browser (appRoot)
--- src/app.js
+++ src/app.js
@@ -1,7 +1,28 @@
 console.log('App.js is running!')
+//to run during dev
+//babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
+//live-server public
 
 // JSX - JavaScript XML
-var template = <h1>Indecision App</h1>
+var template = ( // wrapping parantheses only for readability, to remove an open-ended '='
+<div>
+    <h1>Indecision App</h1> 
+    <p>This is some cool info</p>
+    <ol>
+        <li>Item one</li>
+        <li>Item two</li>
+    </ol>
+</div>
+)
+
+var templateTwo = (
+<div>
+    <h1>David Arinder</h1>
+    <p>Location: Seattle</p>
+</div>
+)
+
+
 var appRoot = document.getElementById('app')
 
-ReactDOM.render(template, appRoot) // render this element (template) into this part of the browser (appRoot)
+ReactDOM.render(templateTwo, appRoot) // render this element (template) into this part of the browser (appRoot)
